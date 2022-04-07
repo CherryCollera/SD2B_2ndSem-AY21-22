c5dfc68b6c180224278ffca2c22aec6cb60041a9
Cheri <cherrycollera@gmail.com>
2022-04-07 14:27:56 +0800

    Add files via upload

---
+++ Activity2_Collera/Activity2_Collera/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Activity2_Collera
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Activity2_Collera/MyProfile_Collera/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyProfile_Collera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\tName:\t\tCherry A. Collera\n\n");
+            Console.WriteLine("\tDate of Birth:\tFebruary 28\n");
+            Console.WriteLine("\tCourse:\t\tB.S. Computer Information System\n");
+            Console.WriteLine("\tYear:\t\tIV\n");
+            Console.WriteLine("\tSection:\t401\n");
+            Console.ReadKey();
+        }
+    }
+}

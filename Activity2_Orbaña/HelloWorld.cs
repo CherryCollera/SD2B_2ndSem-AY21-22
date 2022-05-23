843955b5901c469bf1f080514807de53cadf5123
Sofia Orbaña <94208317+mssofiao@users.noreply.github.com>
2022-05-23 20:47:11 +0800

    Add files via upload

---
+++ Activity2_Orbaña/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Activity2_Orbaña
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.ReadKey();
+        }
+    }
+}
